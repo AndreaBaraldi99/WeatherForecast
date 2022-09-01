275f1f477905f6867a8909f96eb5395a6b1c218e
AndreaBaraldi99 <andrea.baraldi99@hotmail.it>
2022-09-01 21:21:32 +0200

    Weather details
    Added the "Daily weather" column that converts the weather code to a weather description

--- src/Services/Weather/Daily.java
+++ src/Services/Weather/Daily.java
@@ -11,4 +11,102 @@ public class Daily{
     public ArrayList<Double> windspeed_10m_max;
     public ArrayList<Integer> weathercode;
     
+    public String[] getWeather() {
+    	int k = 0;
+    	String[] weather = new String[weathercode.size()];
+    	for(Integer i: weathercode) {
+    		switch(i) {
+    			case 0:
+    				weather[k]="Clear Sky";
+    				break;
+    			case 1:
+    				weather[k]="Mainly clear";
+    				break;
+    			case 2:
+    				weather[k]="Partly cloudy";
+    				break;
+    			case 3:
+    				weather[k]="Overcast";
+    				break;
+    			case 45:
+    				weather[k]="Fog";
+    				break;
+    			case 48:
+    				weather[k]="Depositing rime fog";
+    				break;
+    			case 51:
+    				weather[k]="Light drizzle";
+    				break;
+    			case 53:
+    				weather[k]="Moderate drizzle";
+    				break;
+    			case 55:
+    				weather[k]="Dense drizzle";
+    				break;
+    			case 56:
+    				weather[k]="Light freezing drizzle";
+    				break;
+    			case 57:
+    				weather[k]="Dense freezing drizzle";
+    				break;
+    			case 61:
+    				weather[k]="Slight rain";
+    				break;
+    			case 63:
+    				weather[k]="Moderate rain";
+    				break;
+    			case 65:
+    				weather[k]="Heavy rain";
+    				break;
+    			case 66:
+    				weather[k]="Light freezing rain";
+    				break;
+    			case 67:
+    				weather[k]="Heavy freezing rain";
+    				break;
+    			case 71:
+    				weather[k]="Slight snow fall";
+    				break;
+    			case 73:
+    				weather[k]="Moderate snow fall";
+    				break;
+    			case 75:
+    				weather[k]="Heavy snow fall";
+    				break;
+    			case 77:
+    				weather[k]="Snow grains";
+    				break;
+    			case 80:
+    				weather[k]="Slight rain showers";
+    				break;
+    			case 81:
+    				weather[k]="Moderate rain showers";
+    				break;
+    			case 82:
+    				weather[k]="Violent rain showers";
+    				break;
+    			case 85:
+    				weather[k]="Slight snow showers";
+    				break;
+    			case 86:
+    				weather[k]="Heavy snow showers";
+    				break;
+    			case 95:
+    				weather[k]="Thunderstorm";
+    				break;
+    			case 96:
+    				weather[k]="Thunderstorm with slight hail";
+    				break;
+    			case 99:
+    				weather[k]="Thunderstorm with heavy hail";
+    				break;
+    			default:
+    				weather[k]="No data";
+    				break;
+    		}
+    		k++;
+    	}
+    	return weather;
+    }
+    
 }
--- src/Services/Weather/WeatherForecastResult.java
+++ src/Services/Weather/WeatherForecastResult.java
@@ -22,18 +22,18 @@ public class WeatherForecastResult{
     }
     
     public String toWeatherTable() {
-    	String leftAlignFormat = "| %-10s | %-8s | %-8s | %-6s | %-7s | %-7s | %-8s | %-5s |\n";
+    	String leftAlignFormat = "| %-10s | %-8s | %-8s | %-7s | %-6s | %-7s | %-8s | %-29s |\n";
     	String table = "";
-    	table+=("+------------+----------+----------+--------+---------+---------+----------+-------+\n");
-    	table+=("| Date       | Max temp | Min Temp | Sunset | Sunrise | Rain mm | Wind spd | Wc    |\n");
-    	table+=("+------------+----------+----------+--------+---------+---------+----------+-------+\n");
+    	table+=("+------------+----------+----------+---------+--------+---------+----------+-------------------------------+\n");
+    	table+=("| Date       | Max temp | Min Temp | Sunrise | Sunset | Rain mm | Wind spd |         Daily weather         |\n");
+    	table+=("+------------+----------+----------+---------+--------+---------+----------+-------------------------------+\n");
     	for(int i = 0; i<daily.time.size(); i++) {
     		String row = String.format(leftAlignFormat, daily.time.get(i), daily.temperature_2m_max.get(i), daily.temperature_2m_min.get(i), 
-    				daily.sunset.get(i).substring(daily.sunset.get(i).indexOf("T")+1), daily.sunrise.get(i).substring(daily.sunset.get(i).indexOf("T")+1),
-    				daily.precipitation_sum.get(i), daily.windspeed_10m_max.get(i), daily.weathercode.get(i));
+    				daily.sunrise.get(i).substring(daily.sunset.get(i).indexOf("T")+1), daily.sunset.get(i).substring(daily.sunset.get(i).indexOf("T")+1),
+    				daily.precipitation_sum.get(i), daily.windspeed_10m_max.get(i), daily.getWeather()[i]);
     		table += row;
     	}
-    	table+=("+------------+----------+----------+--------+---------+---------+----------+-------+\n");
+    	table+=("+------------+----------+----------+---------+--------+---------+----------+-------------------------------+\n");
     	
     	return table;
     	
